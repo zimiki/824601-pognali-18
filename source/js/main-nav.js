af95852d1aa347559880c4e77ece3e43abc4b536
zimiki <marmonka@gmail.com>
2019-11-27 19:33:58 +0500

    Оформлет page-header

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,14 +1,14 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('page-header--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('page-header--closed')) {
+    navMain.classList.remove('page-header--closed');
+    navMain.classList.add('page-header--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('page-header--closed');
+    navMain.classList.remove('page-header--opened');
   }
 });
