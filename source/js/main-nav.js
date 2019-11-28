c89770f306e17df042423ebac28d34d64edf371a
zimiki <marmonka@gmail.com>
2019-11-28 21:18:01 +0500

    Добавляет JS на page-header

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,6 +2,8 @@ var navMain = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
 
 navMain.classList.remove('page-header--nojs');
+navMain.classList.remove('page-header--opened');
+navMain.classList.add('page-header--closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('page-header--closed')) {
